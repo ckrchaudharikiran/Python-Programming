ff603b6fcf5c0cc599464b1718bbe3145764bd15
Kiran R Chaudhari <100767110+ckrchaudharikiran@users.noreply.github.com>
2026-02-14 16:32:14 +0530

    Create array_loops.py

---
+++ array_loops.py
@@ -0,0 +1,25 @@
+import array as arr
+a = arr.array('d', [56, 42, 23, 85, 45])
+l = len(a)
+for x in range(l):
+   print (a[x])
+
+
+# import array as arr
+# # creating array
+# a = arr.array('i', [96, 26, 56, 76, 46])
+# # checking the length
+# l = len(a)
+# # loop variable
+# idx = 0
+# # while loop
+# while idx < l:
+#    print (a[idx])
+#    # incrementing the while loop
+#    idx+=1
+
+
+# import array as arr
+# newArray = arr.array('i', [56, 42, 23, 85, 45])
+# for iterate in newArray:
+#    print (iterate)
