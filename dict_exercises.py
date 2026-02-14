c341a61731fa477adc13eea7ecd6892d8f309f13
Kiran R Chaudhari <100767110+ckrchaudharikiran@users.noreply.github.com>
2026-02-14 13:46:05 +0530

    Create dict_exercises.py

---
+++ dict_exercises.py
@@ -0,0 +1,64 @@
+# Python program to sort list of dictionaries by values
+
+# Python program to extract dictionary with each key having non-numeric value from a given dictionary.
+
+# Python program to build a dictionary from list of two item (k,v) tuples.
+
+# Python program to merge two dictionary objects, using unpack operator.
+
+
+#   employees = [
+#     {"name": "Alice", "salary": 50000},
+#     {"name": "Bob", "salary": 70000},
+#     {"name": "Charlie", "salary": 60000}
+# ]
+
+# # Sort by salary (ascending)
+# sorted_list = sorted(employees, key=lambda x: x["salary"])
+
+# print(sorted_list)
+
+
+# sorted_list = sorted(employees, key=lambda x: x["salary"], reverse=True)
+
+
+
+# data = {
+#     "a": 10,
+#     "b": "hello",
+#     "c": 3.14,
+#     "d": "world",
+#     "e": True
+# }
+
+# # Filter non-numeric values
+# result = {k: v for k, v in data.items() if not isinstance(v, (int, float))}
+
+# print(result)
+
+
+
+# # Filter non-numeric values
+# result = {k: v for k, v in data.items() if not isinstance(v, (int, float))}
+
+# print(result)
+
+
+# # Filter non-numeric values
+# result = {k: v for k, v in data.items() if not isinstance(v, (int, float))}
+# print(result)
+
+
+# dict1 = {"a": 1, "b": 2}
+# dict2 = {"c": 3, "d": 4}
+# merged_dict = {**dict1, **dict2}
+# print(merged_dict)
+
+
+
+# dict1 = {"a": 1, "b": 2}
+# dict2 = {"b": 20, "c": 3}
+
+# merged_dict = {**dict1, **dict2}
+
+# print(merged_dict)
