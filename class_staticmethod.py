a59a9f8bc5f1523bd674012819571264f49c3b64
Kiran R Chaudhari <100767110+ckrchaudharikiran@users.noreply.github.com>
2026-02-15 22:11:09 +0530

    Create class_staticmethod.py

---
+++ class_staticmethod.py
@@ -0,0 +1,40 @@
+class Employee:
+   empCount = 0
+   def __init__(self, name, age):
+      self.__name = name
+      self.__age = age
+      Employee.empCount += 1
+   
+   # creating staticmethod
+   def showcount():
+      print (Employee.empCount)
+      return
+   counter = staticmethod(showcount)
+
+e1 = Employee("Bhavana", 24)
+e2 = Employee("Rajesh", 26)
+e3 = Employee("John", 27)
+
+e1.counter()
+Employee.counter()
+
+
+
+# class Student:
+#    stdCount = 0
+#    def __init__(self, name, age):
+#       self.__name = name
+#       self.__age = age
+#       Student.stdCount += 1
+   
+#    # creating staticmethod
+#    @staticmethod
+#    def showcount():
+#       print (Student.stdCount)
+
+# e1 = Student("Bhavana", 24)
+# e2 = Student("Rajesh", 26)
+# e3 = Student("John", 27)
+
+# print("Number of Students:")
+# Student.showcount()
