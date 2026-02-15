19bdb2be01078349084f39cacc4894c1889fe66b
Kiran R Chaudhari <100767110+ckrchaudharikiran@users.noreply.github.com>
2026-02-15 10:23:38 +0530

    Create exception_handling_2nd.py

---
+++ exception_handling_2nd.py
@@ -0,0 +1,79 @@
+try:
+   file = open("example.txt", "r")
+   content = file.read()
+   print(content)
+except FileNotFoundError:
+   print("Error: The file was not found.")
+else:
+   print("File read operation successful.")
+finally:
+   if 'file' in locals():
+      file.close()
+   print("File operation is complete.")
+
+
+# try:
+#    file = open("example.txt", "r")
+#    content = file.read()
+#    print(content)
+# except FileNotFoundError:
+#    print("Error: The file was not found.")
+# else:
+#    print("File read operation successful.")
+# finally:
+#    if 'file' in locals():
+#       file.close()
+#    print("File operation is complete.")
+
+
+
+# def divide(a, b):
+#    if b == 0:
+#       raise ValueError("Cannot divide by zero")
+#    return a / b
+# try:
+#    result = divide(10, 0)
+# except ValueError as e:
+#    print(e)
+
+
+# class MyCustomError(Exception):
+#    pass
+# def risky_function():
+#    raise MyCustomError("Something went wrong in risky_function")
+# try:
+#    risky_function()
+# except MyCustomError as e:
+#    print(e)
+
+
+
+# class InvalidAgeError(Exception):
+#    def __init__(self, age, message="Age must be between 18 and 100"):
+#       self.age = age
+#       self.message = message
+#       super().__init__(self.message)
+# def set_age(age):
+#    if age < 18 or age > 100:
+#       raise InvalidAgeError(age)
+#    print(f"Age is set to {age}")
+# try:
+#    set_age(150)
+# except InvalidAgeError as e:
+#    print(f"Invalid age: {e.age}. {e.message}")
+
+
+
+# def process_file(filename):
+#    try:
+#       with open(filename, "r") as file:
+#          data = file.read()
+#          # Process data
+#    except FileNotFoundError as e:
+#       print(f"File not found: {filename}")
+#     	# Re-raise the exception
+#       raise  
+# try:
+#    process_file("nonexistentfile.txt")
+# except FileNotFoundError as e:
+#    print("Handling the exception at a higher level")
