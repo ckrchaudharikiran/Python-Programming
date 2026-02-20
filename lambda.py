06521463075d9aa62eaec87e053cf9776d697099
Kiran R Chaudhari <100767110+ckrchaudharikiran@users.noreply.github.com>
2026-02-20 18:09:21 +0530

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,16 @@
+add = lambda a, b: a + b# 
+
+
+# array = [1, 2, 3]
+# result = list(map(lambda x: x ** 2, array))
+# print(result)
+
+
+# array = [5,10,15,20]
+# result = list(filter(lambda x: x % 2 == 0, array))
+# print(result)
+
+
+# array = ['Ravi', 'Ram', 'Ravan', 'Richel']
+# result = sorted(array, key=lambda x: len(x))
+# print(result)
