7accaa4071032fa4ae412d77bbcf36b0e5e5cc25
Kiran R Chaudhari <100767110+ckrchaudharikiran@users.noreply.github.com>
2026-02-23 23:40:52 +0530

    Update polymorphism_duck_typing.py

--- polymorphism_duck_typing.py
+++ polymorphism_duck_typing.py
@@ -1,10 +1,26 @@
-class Dog:
-    def speak(self):
-        return "Bark"
+def get_area(shape):
+    return shape.area()
 
-class Cat:
-    def speak(self):
-        return "Meow"
+class Circle:
+    def area(self):
+        return 3.14 * 5 * 5
 
-def make_sound(animal):
-    print(animal.speak())
+class Square:
+    def area(self):
+        return 4 * 4
+
+print(get_area(Circle()))
+print(get_area(Square()))
+
+
+
+# class Dog:
+#     def speak(self):
+#         return "Bark"
+
+# class Cat:
+#     def speak(self):
+#         return "Meow"
+
+# def make_sound(animal):
+#     print(animal.speak())
